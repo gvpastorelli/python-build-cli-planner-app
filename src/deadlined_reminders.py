71de0bf3168d670d3bdeb4d9fe3b579c8a56b6ef
Gregory V Pastorelli <gpastorelli@gmail.com>
2021-12-26 20:37:01 -0500

    init abc

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,5 +1,7 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, ABCMeta, abstractmethod
 from collections.abc import Iterable
+from dateutil.parser import parse
+from datetime import datetime
 
 class DeadlinedMetaReminder(Iterable,metaclass=ABCMeta):
 
@@ -7,6 +9,18 @@ class DeadlinedMetaReminder(Iterable,metaclass=ABCMeta):
     def is_due(self):
         pass
 
+class DeadlinedReminder(ABC, Iterable):
+
+    @abstractmethod
+    def is_due(self):
+        pass
+
+class DateReminder(DeadlinedReminder):
+
+    def __init__(self, text, date):
+        self.date = parse(date, dayfirst=True)
+        self.text = text
+
 # Create a class named DeadlinedMetaReminder that inherits from Iterable and 
 # takes ABCMeta as its metaclass parameter. Add method is_due(), set the body to 
-# pass and mark it with the @abstractmethod decorator.
+# pass and mark it with the @abstractmethod decorator
