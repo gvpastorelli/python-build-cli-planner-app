ceeed48cab654b9508528596e956de6804f848cb
Gregory V Pastorelli <gpastorelli@gmail.com>
2021-12-26 20:18:44 -0500

    init abc

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,8 +1,11 @@
 from abc import ABCMeta, abstractmethod
 from collections.abc import Iterable
 
-class DeadlinedMetaReminder(Iterable):
-    pass
+class DeadlinedMetaReminder(Iterable,metaclass=ABCMeta):
+
+    @abstractmethod
+    def is_due(self):
+        pass
 
 # Create a class named DeadlinedMetaReminder that inherits from Iterable and 
 # takes ABCMeta as its metaclass parameter. Add method is_due(), set the body to 
